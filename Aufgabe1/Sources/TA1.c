a6603a51d1931eb9e8af2e3116a610abeed0e914
corneliusbokelmann <64590911+corneliusbokelmann@users.noreply.github.com>
2023-04-25 12:12:22 +0200

    Update TA1.c

--- Aufgabe1/Sources/TA1.c
+++ Aufgabe1/Sources/TA1.c
@@ -32,4 +32,68 @@ __interrupt Void TIMER1_A1_ISR(Void) {
     * Der Inhalt der ISR ist zu implementieren
     */
     
+    static volatile UInt state1 = 0;
+    static volatile UInt cnt1 = 0;
+    #define N_BOUNCING 4
+    //Button1
+    if(TSTBIT(P1IN, BIT0)) {
+        if(state1 == 0) {
+            if(cnt1 < N_BOUNCING - 1) {
+                cnt1++;
+            } else if(cnt1 == N_BOUNCING - 1) {
+                state1 = 1;
+                Event_set(EVENT_BTN1);
+                __low_power_mode_off_on_exit();
+            }
+        } else if(state1 == 1) {
+            if(cnt1 < N_BOUNCING - 1) {
+                cnt1++;
+            }
+        }
+    } else {
+        if(state1 == 0) {
+            if(cnt1 > 0) {
+                cnt1--;
+            }
+        } else if(state1 == 1) {
+            if(cnt1 > 0) {
+                cnt1--;
+            } else if(cnt1 == 0) {
+                state1 = 0;
+            }
+        }
+    }
+    //Button2
+    static volatile UInt state2 = 0;
+    static volatile UInt cnt2 = 0;
+    if(TSTBIT(P1IN, BIT1)) {
+        if(state2 == 0) {
+            if(cnt2 < N_BOUNCING - 1) {
+                cnt2++;
+            } else if(cnt2 == N_BOUNCING - 1) {
+                state2 = 1;
+                Event_set(EVENT_BTN2);
+                __low_power_mode_off_on_exit();
+            }
+        } else if(state2 == 1) {
+            if(cnt2 < N_BOUNCING - 1) {
+                cnt2++;
+            }
+        }
+    } else {
+        if(state2 == 0) {
+            if(cnt2 > 0) {
+                cnt2--;
+            }
+        } else if(state2 == 1) {
+            if(cnt2 > 0) {
+                cnt2--;
+            } else if(cnt2 == 0) {
+                state2 = 0;
+            }
+        }
+    }
+
+
+
 }
