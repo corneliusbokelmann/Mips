3d12b08d9cf9dd11b62bfa2574bcd7122b41fca3
corneliusbokelmann <64590911+corneliusbokelmann@users.noreply.github.com>
2023-04-25 11:21:07 +0200

    Update TA0.c

--- Aufgabe1/Sources/TA0.c
+++ Aufgabe1/Sources/TA0.c
@@ -3,33 +3,42 @@
 #include "TA0.h"
 
 /*
- * Man soll sich eine geeignete Datenstruktur überlegen,
- * die eine laufzeiteffiziente Ausführung der ISR ermöglicht.
+ * Man soll sich eine geeignete Datenstruktur Ã¼berlegen,
+ * die eine laufzeiteffiziente AusfÃ¼hrung der ISR ermÃ¶glicht.
  */
-const static UInt TWINKLE_1[2] = {200, 50};
-const static UInt TWINKLE_2[2] = {75, 75};
-const static UInt TWINKLE_3[2] = {25, 25};
-const static UInt TWINKLE_4[2] = {50, 200};
-const static UInt TWINKLE_5[4] = {50, 50, 50, 200};
-const static UInt TWINKLE_6[6] = {50, 50, 50, 50, 50, 200};
 
-const static UInt *TIME_FOR_TWINKLE[6] = {TWINKLE_1, TWINKLE_2, TWINKLE_3, TWINKLE_4, TWINKLE_5, TWINKLE_6};
-const static UInt size[6] = {2,2,2,2,4,6};
 
-static UInt twinkle_pattern;
-static UInt current_twinkle_pattern;
-static UInt twinkle_phase_count = 0;
-static UInt twinkle_time_count = 0;
-static UInt period_limit;
+#define HIGH 0x8000
+#define LOW 0x0000
+#define ACKFRQ 614.4 // kHz
+#define TIMEBASE 250 // ms
+#define SCALING ((UInt)(ACKFRQ * TIMEBASE))
+#define TICK(t) (((SCALING / 8) / 8) * ((t) / TIMEBASE) - 1) // TODO: proper scaling!!!
+
+const static UInt muster_1[2] = {HIGH | TICK(200), LOW | TICK(50), 0};
+const static UInt muster_2[2] = {HIGH | TICK(75), LOW | TICK(75), 0};
+const static UInt muster_3[2] = {HIGH | TICK(25), LOW | TICK(25), 0};
+const static UInt muster_4[2] = {HIGH | TICK(50), LOW | TICK(200)};
+const static UInt muster_5[4] = {HIGH | TICK(50), LOW | TICK(50), HIGH | TICK(50), LOW | TICK(200), 0};
+const static UInt muster_6[6] = {HIGH | TICK(50), LOW | TICK(50), HIGH | TICK(50), LOW | TICK(50), HIGH | TICK(50), LOW | TICK(200), 0};
+
+const static UInt *muster[6] = {muster_1, muster_2, muster_3, muster_4, muster_5, muster_6};
+const static UInt size[6] = {3,3,3,3,5,7};
+
+static UInt current_pattern_idx;
+
+LOCAL const Int *ptr;
+
 
 GLOBAL Void set_blink_muster(UInt arg) {
 /*
  * Die Funktion muss so erweitert werden,
  * dass ein Blinkmuster selektiert wird.
- * Diese Lösung hängt stark von der gewählten
+ * Diese LÃ¶sung hÃ¤ngt stark von der gewÃ¤hlten
  * Datenstruktur ab.
  */
-    twinkle_pattern = arg;
+    
+    current_pattern_idx = arg;
 }
 
 #pragma FUNC_ALWAYS_INLINE(TA0_init)
@@ -50,95 +59,30 @@ GLOBAL Void TA0_init(Void) {
             | TACLR;          // clear and start Timer
    SETBIT(TA0CTL, TAIE        // enable interrupt
                 | TAIFG);     // set interrupt flag
-
-   twinkle_pattern = 0;
-      current_twinkle_pattern = twinkle_pattern;
-      period_limit = TIME_FOR_TWINKLE[current_twinkle_pattern][0];
-      SETBIT(P1OUT, BIT2);
 }
 
 #pragma vector = TIMER0_A1_VECTOR
 __interrupt Void TIMER0_A1_ISR(Void) {
-    //Button entprellen
-    static volatile UInt bouncing_state1 = 0;
-    static volatile UInt cnt1 = 0;
-    #define N_BOUNCING 4
-    //Button1
-    if(TSTBIT(P1IN, BIT0)) {
-        if(bouncing_state1 == 0) {
-            if(cnt1 < N_BOUNCING - 1) {
-                cnt1++;
-            } else if(cnt1 == N_BOUNCING - 1) {
-                bouncing_state1 = 1;
-                set_event(EVENT_BTN1);
-                __low_power_mode_off_on_exit();
-            }
-        } else if(bouncing_state1 == 1) {
-            if(cnt1 < N_BOUNCING - 1) {
-                cnt1++;
-            }
-        }
-    } else {
-        if(bouncing_state1 == 0) {
-            if(cnt1 > 0) {
-                cnt1--;
-            }
-        } else if(bouncing_state1 == 1) {
-            if(cnt1 > 0) {
-                cnt1--;
-            } else if(cnt1 == 0) {
-                bouncing_state1 = 0;
-            }
-        }
-    }
-    //Button2
-    static volatile UInt bouncing_state2 = 0;
-    static volatile UInt cnt2 = 0;
-    if(TSTBIT(P1IN, BIT1)) {
-        if(bouncing_state2 == 0) {
-            if(cnt2 < N_BOUNCING - 1) {
-                cnt2++;
-            } else if(cnt2 == N_BOUNCING - 1) {
-                bouncing_state2 = 1;
-                set_event(EVENT_BTN2);
-                __low_power_mode_off_on_exit();
-            }
-        } else if(bouncing_state2 == 1) {
-            if(cnt2 < N_BOUNCING - 1) {
-                cnt2++;
-            }
-        }
-    } else {
-        if(bouncing_state2 == 0) {
-            if(cnt2 > 0) {
-                cnt2--;
-            }
-        } else if(bouncing_state2 == 1) {
-            if(cnt2 > 0) {
-                cnt2--;
-            } else if(cnt2 == 0) {
-                bouncing_state2 = 0;
-            }
-        }
-    }
-
-    //blinken
-    twinkle_time_count += 1;
-    //Schaltzyklus beendet?
-    if(twinkle_time_count == period_limit) {
-        TGLBIT(P1OUT, BIT2);
-        twinkle_time_count = 0;
-        twinkle_phase_count += 1;
-        //Ende des Musters erreicht
-        if (twinkle_phase_count == size[current_twinkle_pattern]) {
-            twinkle_phase_count = 0;
-            //Falls Taster gedrückt, nächstes Muster
-            current_twinkle_pattern = twinkle_pattern;
-        }
-        period_limit = TIME_FOR_TWINKLE[current_twinkle_pattern][twinkle_phase_count];
-    }
 
    /*
     * Der Inhalt der ISR ist zu implementieren
     */
+    
+    UInt cnt = *ptr++;
+    
+    Int pattern[size[current_pattern_idx]] = muster[current_pattern_idx]
+    
+    if(TSTBIT(cnt, HIGH)) {
+        SETBIT(P2OUT, BIT7);
+    } else {
+        CLRBIT(P2OUT, BIT7);
+    }
+    
+    CLRBIT(TA0CTL, TAIFG);
+    TA0CCR0 = ~HIGH BAND cnt;
+    
+    if(*ptr EQ 0) {
+        ptr = pattern[0];
+    }
+       
 }
