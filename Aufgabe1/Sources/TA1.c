b7de390a76994391d9e89075c850ae4549402fb1
corneliusbokelmann <64590911+corneliusbokelmann@users.noreply.github.com>
2023-05-06 12:24:02 +0200

    Update TA1.c

--- Aufgabe1/Sources/TA1.c
+++ Aufgabe1/Sources/TA1.c
@@ -2,98 +2,75 @@
 #include "..\base.h"
 #include "TA1.h"
 #include "event.h"
+#include <stdio.h>
+#define THRSH 4
+
+UInt count_1 = 0;
+UInt count_2 = 0;
+UInt state_1 = 0;
+UInt state_2 = 0;
+
+
 
 
 #pragma FUNC_ALWAYS_INLINE(TA1_init)
-GLOBAL Void TA1_init(Void) {
-
-   CLRBIT(TA1CTL,   MC0 | MC1  // stop mode
-                  | TAIE       // disable interrupt
-                  | TAIFG);    // clear interrupt flag
-   CLRBIT(TA1CCTL0, CM1 | CM0  // no capture mode
-                  | CAP        // compare mode
-                  | CCIE       // disable interrupt
-                  | CCIFG);    // clear interrupt flag
-   TA1CCR0  = 96-1;            // set up Compare Register
-   TA1EX0   = TAIDEX_7;        // set up expansion register
-   TA1CTL   = TASSEL__ACLK     // 614.4 kHz
-            | MC__UP           // Up Mode
-            | ID__8            // /8
-            | TACLR;           // clear and start Timer
-   SETBIT(TA1CTL, TAIE);       // enable interrupt
+GLOBAL Void TA1_init(Void)
+{
+   CLRBIT(TA1CTL, MC0 | MC1       // stop mode
+                      | TAIE      // disable interrupt
+                      | TAIFG);   // clear interrupt flag
+   CLRBIT(TA1CCTL0, CM1 | CM0     // no capture mode
+                        | CAP     // compare mode
+                        | CCIE    // disable interrupt
+                        | CCIFG); // clear interrupt flag
+   TA1CCR0 = 96 - 1;              // set up Compare Register
+   TA1EX0 = TAIDEX_7;             // set up expansion register
+   TA1CTL = TASSEL__ACLK          // 614.4 kHz
+            | MC__UP              // Up Mode
+            | ID__8               // /8
+            | TACLR;              // clear and start Timer
+   SETBIT(TA1CTL, TAIE);          // enable interrupt
 }
 
-
-
 #pragma vector = TIMER1_A1_VECTOR
-__interrupt Void TIMER1_A1_ISR(Void) {
-
-   /*
-    * Der Inhalt der ISR ist zu implementieren
-    */
-    
-    static volatile UInt state1 = 0;
-    static volatile UInt cnt1 = 0;
-    #define N_BOUNCING 4
-    //Button1
-    if(TSTBIT(P1IN, BIT0)) {
-        if(state1 == 0) {
-            if(cnt1 < N_BOUNCING - 1) {
-                cnt1++;
-            } else if(cnt1 == N_BOUNCING - 1) {
-                state1 = 1;
-                Event_set(EVENT_BTN1);
-                __low_power_mode_off_on_exit();
+__interrupt Void TIMER1_A1_ISR(Void)
+{
+    if(TSTBIT(P1IN, BIT1) && count_1 LT 5) {
+        count_1++;
+        } else if(count_1 GT 0) {
+            count_1--;
+        }
+        if (!state_1) {
+            if (count_1 EQ THRSH) {
+                state_1 = 1;
+                // send button event
+                Event_set(EVENT_BTN1); // set up event
             }
-        } else if(state1 == 1) {
-            if(cnt1 < N_BOUNCING - 1) {
-                cnt1++;
+        } else {
+            if (count_1 EQ 0) {
+                state_1 = 0;
             }
         }
-    } else {
-        if(state1 == 0) {
-            if(cnt1 > 0) {
-                cnt1--;
+
+        //btn2
+
+        if(TSTBIT(P1IN, BIT0) && count_2 LT 5) {
+            count_2++;
+        } else if(count_2 GT 0) {
+            count_2--;
+        }
+        if (!state_2) {
+            if (count_2 EQ THRSH) {
+                state_2 = 1;
+                // send button event
+                Event_set(EVENT_BTN2); // set up event
             }
-        } else if(state1 == 1) {
-            if(cnt1 > 0) {
-                cnt1--;
-            } else if(cnt1 == 0) {
-                state1 = 0;
+        } else {
+            if (count_2 EQ 0) {
+                state_2 = 0;
             }
         }
-    }
-    //Button2
-    static volatile UInt state2 = 0;
-    static volatile UInt cnt2 = 0;
-    if(TSTBIT(P1IN, BIT1)) {
-        if(state2 == 0) {
-            if(cnt2 < N_BOUNCING - 1) {
-                cnt2++;
-            } else if(cnt2 == N_BOUNCING - 1) {
-                state2 = 1;
-                Event_set(EVENT_BTN2);
-                __low_power_mode_off_on_exit();
-            }
-        } else if(state2 == 1) {
-            if(cnt2 < N_BOUNCING - 1) {
-                cnt2++;
-            }
-        }
-    } else {
-        if(state2 == 0) {
-            if(cnt2 > 0) {
-                cnt2--;
-            }
-        } else if(state2 == 1) {
-            if(cnt2 > 0) {
-                cnt2--;
-            } else if(cnt2 == 0) {
-                state2 = 0;
-            }
-        }
-    }
 
-
-
+        CLRBIT(TA1CTL, TAIFG); // clear interrupt flag
+        __low_power_mode_off_on_exit(); // restore Active Mode on return
 }
