a68e80253e296577530451054af5768cc60f5f9a
corneliusbokelmann <64590911+corneliusbokelmann@users.noreply.github.com>
2023-06-06 12:40:24 +0200

    Update Handler.c

--- Aufgabe2/Sources/Handler.c
+++ Aufgabe2/Sources/Handler.c
@@ -2,93 +2,116 @@
 #include "Handler.h"
 #include "event.h"
 #include "UCA1.h"
+#include "TA0.h"
 
 #define DIGISIZE 4
-#define BASE     10 // Basis des Zahlensystems kann zwischen 2 und 16 gewählt werden
+#define BASE     10
 
-static UInt count[4] = {0, 0, 0, 0};
 
-// TODO: Events anpassen
-
-LOCAL Void add(UInt idx) {
-    if(count[idx] == BASE - 1) { //Plus
-        count[idx] = 0;
-        if (idx != 3) {
-            add(idx + 1);
-        }
-    } else {
-        count[idx] += 1;
-    }
-}
-
-LOCAL Void sub(UInt idx) {
-    if(count[idx] == 0) {
-        count[idx] = BASE - 1;
-        if (idx != 3) {
-            sub(idx + 1);
-        }
-    } else {
-        count[idx] -= 1;
-    }
-}
-
-LOCAL Void update_number(UInt idx) {
-    if(TSTBIT(P2OUT, BIT7)) {
-        sub(idx);
-    } else {
-        add(idx);
-    }
-
-    Event_set(EVENT_DIGI);
-}
-
+UChar select;
+signed char digits[DIGISIZE];
+UChar i;
+LOCAL Int cnt;
 // ----------------------------------------------------------------------------
 
 GLOBAL Void Button_Handler(Void) {
-    static UInt idx = 0;
-        UInt evt = EVENT_BTN3 << idx;
-        if(Event_tst(evt)) {
-            Event_clr(evt);
-            update_number(idx);
-        }
 
-        idx++;
-        if(idx == 4) {
-            idx = 0;
+    if (Event_tst(EVENT_BTN1)) {
+        Event_clr(EVENT_BTN1);
+        TGLBIT(P2OUT, BIT7);
+    }  else if (Event_tst(EVENT_BTN2)) {
+        Event_clr(EVENT_BTN2);
+        if (++cnt GT MUSTER6) {
+           cnt = MUSTER1;
         }
+        set_blink_muster(cnt);
+     }
 
+    else if (Event_tst(EVENT_BTN3)) {
+        Event_clr(EVENT_BTN3);
+        select = 0;
+        Event_set(EVENT_15);
+    } else if (Event_tst(EVENT_BTN4)) {
+        Event_clr(EVENT_BTN4);
+        select = 1;
+        Event_set(EVENT_15);
+    } else if (Event_tst(EVENT_BTN5)) {
+        Event_clr(EVENT_BTN5);
+        select = 2;
+        Event_set(EVENT_15);
+    } else if (Event_tst(EVENT_BTN6)) {
+        Event_clr(EVENT_BTN6);
+        select = 3;
+        Event_set(EVENT_15);
+    }
 }
 
 // ----------------------------------------------------------------------------
 
 
 GLOBAL Void Number_Handler(Void) {
-    if(Event_tst(EVENT_DIGI)) {
-        Event_clr(EVENT_DIGI);
-        // Digits anzeigen mit UCA1.emit
-            //display digits
-        if(tst_event(EVENT_DIGI)) {
-            clr_event(EVENT_DIGI);
-            emit(0x01, count[0]);
-            emit(0x02, count[1]);
-            emit(0x03, count[2]);
-            emit(0x04, count[3]);
-            emit(0x0B, 0x03); //display digits
+    if (Event_tst(EVENT_15)) {
+       Event_clr(EVENT_15);
+       if (TSTBIT(P2OUT, BIT7)) {
+           digits[select] -= 1;
+       } else {
+           digits[select] += 1;
+       }
+       Event_set(EVENT_RUN);
+   }
+}
+
+// ----------------------------------------------------------------------------
+
+// Datentyp eines Funktionspointers
+typedef Void (* VoidFunc)(Void);
+
+// Funktionsprototypen
+LOCAL Void State0(Void);
+LOCAL Void State1(Void);
+
+// lokale Zustandsvariable
+LOCAL VoidFunc state;
+
+LOCAL Void State0(Void) {
+    if (Event_tst(EVENT_RUN)) {
+        Event_clr(EVENT_RUN);
+        i = 1;
+        state = State1;
+        Event_set(EVENT_DONE);
+    }
+}
+
+LOCAL Void State1(Void) {
+    if (Event_tst(EVENT_DONE)) {
+        Event_clr(EVENT_DONE);
+        if (i LE 4) {
+            UChar index = i - 1;
+            if (digits[index] > BASE-1) {
+                digits[index] = 0;
+                digits[i]++;
+            } else if (digits[index] < 0) {
+                digits[index] = BASE-1;
+                digits[i]--;
+            }
+            UCA1_emit(i, digits[index]);
+            i++;
+        } else {
+            state = State0;
         }
     }
 }
 
-// ----------------------------------------------------------------------------
-
-
-
 GLOBAL Void AS1108_Handler(Void) {
-
+    (*state)();
 }
 
 // ----------------------------------------------------------------------------
-
 GLOBAL Void Handler_init(Void) {
- // TODO: main aufsetzen
+    cnt = MUSTER1;
+    digits[0] = 0;
+    digits[1] = 0;
+    digits[2] = 0;
+    digits[3] = 0;
+    state = State0;
 }
-
